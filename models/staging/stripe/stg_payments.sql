9b3cb847d9c9270e03b5546a689b91f8cdc886fe
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-05-28 21:46:16 +0000

    macro added

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,7 @@ select
     paymentmethod as payment_method,
     status,
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+    {{ cents_to_dollars ('amount') }} as amount,
     created as created_at
 
 from {{ source('stripe', 'payment') }}
